e4378dfc9c4a51736e5d23ddadd5f1dfafcfdc69
shvalade <volodymyr.shakhrai@gmail.com>
2020-04-12 13:24:04 +0300

    Updates

---
+++ precache-manifest.cd11f933b4db4991cff9160f4d8d8479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecad8c98d40614013ec0b2c660992f61",
+    "url": "/study-react/index.html"
+  },
+  {
+    "revision": "f189559f9ee5b8d83cc7",
+    "url": "/study-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2ea45bae2f11f9ac36a4",
+    "url": "/study-react/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "f189559f9ee5b8d83cc7",
+    "url": "/study-react/static/js/2.8cf42406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study-react/static/js/2.8cf42406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea45bae2f11f9ac36a4",
+    "url": "/study-react/static/js/main.16cdd5d9.chunk.js"
+  },
+  {
+    "revision": "1503460985d2e159c644",
+    "url": "/study-react/static/js/runtime-main.b2850644.js"
+  }
+]);
